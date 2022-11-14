49074c8ebeae4b38e0e2aaf60db44b9039a45468
Mark Hobson <markhobson@gmail.com>
2022-11-14 16:04:21 +0000

    Install Bundler Gems locally

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+mkdir -p ~/.bundle
+ln -sf "$(pwd)/.bundle/config" ~/.bundle/config
 ln -sf "$(pwd)/.gemrc" ~/.gemrc
 ln -sf "$(pwd)/.gitconfig" ~/.gitconfig
 ln -sf "$(pwd)/.tigrc" ~/.tigrc
